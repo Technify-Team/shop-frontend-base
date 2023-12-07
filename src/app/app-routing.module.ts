b6fdc0b69acb478a472a356ee7e6e7e46f2b2cf1
Angelina <lina.shkolnaya318@gmail.com>
2023-12-07 18:28:17 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ContainerComponent } from './container/container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'catalog', component: ContainerComponent },
+  { path: 'catalog/:id', component: ProductPageComponent },
+  { path: '', redirectTo:'/catalog', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
