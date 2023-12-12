9d64e70fce37546cbbd3dbbacad3d607734ee085
Angelina <lina.shkolnaya318@gmail.com>
2023-12-12 15:53:17 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ContainerComponent } from './container/container.component';
+import { BasketPageComponent } from './basket-page/basket-page.component';
 
 const routes: Routes = [
   { path: 'catalog', component: ContainerComponent },
   { path: 'catalog/:id', component: ProductPageComponent },
+  { path: 'basket', component: BasketPageComponent },
   { path: '', redirectTo:'/catalog', pathMatch: 'full' },
 ];
 
